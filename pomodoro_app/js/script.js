4cb8cee8ea9b4e29d0a782226c043c418631573b
PiotrekSotor <piotrsotor@gmail.com>
2021-03-21 01:43:35 +0100

    PomodoroApp - rework of start/stop buttons, storage of data in cookies

--- pomodoro_app/js/script.js
+++ pomodoro_app/js/script.js
@@ -4,25 +4,25 @@ var timeCounter = null;
 
 var outputTimer = document.getElementById('timer-time');
 var inputTaskDesc = document.getElementById('task-input');
-
+var standardFontColor = outputTimer.className;
 
 var data = [
-    {
-        period: '12:00 - 12:25',
-        task: 'Reading book'
-    },
-    {
-        period: '11:00 - 11:25',
-        task: 'Watching tutorials'
-    },
-    {
-        period: '10:00 - 10:25',
-        task: 'Exercises'
-    }
+    // {
+    //     period: '12:00 - 12:25',
+    //     task: 'Reading book'
+    // },
+    // {
+    //     period: '11:00 - 11:25',
+    //     task: 'Watching tutorials'
+    // },
+    // {
+    //     period: '10:00 - 10:25',
+    //     task: 'Exercises'
+    // }
 ];
 
 
-
+restoreDataFromCookies();
 setTimeValueOnScreen(lastValidInputPeriod);
 manageTaskList();
 
@@ -34,56 +34,90 @@ inputPeriod.addEventListener('input', e => {
 
     if (e.target.value === '' || /^[0-9]{1,2}[:|-]{0,1}[0-5]{0,1}[0-9]{0,1}$/.test(e.target.value)){
         lastValidInputPeriod = e.target.value;
-        reset();
+        if (!timerInterval){
+            reset();
+        }
     } else {
         e.target.value = lastValidInputPeriod;
     }
 });
+inputPeriod.addEventListener('change', e => {
+    e.target.value = secondsToTime(timeToSeconds(lastValidInputPeriod));
+    reset();
+})
 
 
-var btnStart = document.getElementById('btn-start');
+
+var btnStart = document.getElementById('btn-start-pause');
 btnStart.addEventListener('click', e => {
-    if (timerInterval){
+    var currentFunction = btnStart.innerHTML;
+
+    if (timeCounter === 0) {
         return;
     }
-    if (timeCounter === null || timeCounter <= 0) {
+
+    if (currentFunction === 'Start'){
+        manageTaskList('start');
         timeCounter = timeToSeconds(lastValidInputPeriod);
+        timerInterval = setInterval(() => tick(), 1000);
+        btnStart.innerHTML = 'Pause';
+        
+    }
+    else if (currentFunction === 'Pause'){
+        manageTaskList('pause');
+        clearInterval(timerInterval);
+        timerInterval = null;
+        setTimeValueOnScreen(secondsToTime(timeCounter));
+        btnStart.innerHTML = 'Resume';
+    }
+    else if (currentFunction === 'Resume'){
+        manageTaskList('resume');
+        timerInterval = setInterval(() => tick(), 1000);
+        btnStart.innerHTML = 'Pause';
     }
 
-    manageTaskList('start');
     setTimeValueOnScreen(secondsToTime(timeCounter));
-    timerInterval = setInterval(() => tick(), 1000);
 });
 
+
 function tick(){
-    timeCounter -= 1;
+    if (timeCounter > 0){
+        timeCounter -= 1;
+    }
     setTimeValueOnScreen(secondsToTime(timeCounter));
 }
 
-var btnStop = document.getElementById('btn-stop');
-btnStop.addEventListener('click', e => {
-
-    manageTaskList('pause');
-    clearInterval(timerInterval);
-    timerInterval = null;
-    setTimeValueOnScreen(secondsToTime(timeCounter));
-});
 
 var btnReset = document.getElementById('btn-reset');
 btnReset.addEventListener('click', reset);
 
 function reset(){
     
-    manageTaskList('stop');
+    if (btnStart.innerText != 'Start'){
+        manageTaskList('stop');
+    }
     clearInterval(timerInterval);
     timerInterval = null;
     timeCounter = timeToSeconds(lastValidInputPeriod);
     setTimeValueOnScreen(secondsToTime(timeCounter));
+    btnStart.innerHTML = 'Start';
+    outputTimer.style.color = standardFontColor;
 }
 
 
 
 function setTimeValueOnScreen(timerValue) {
+    if (timerValue === '00:00'){
+        if (outputTimer.className === 'timer-vanished'){
+            outputTimer.className = 'timer';
+        }
+        else {
+            outputTimer.className = 'timer-vanished';
+        }
+    }
+    else {
+        outputTimer.className = 'timer';
+    }
     outputTimer.innerHTML=timerValue;
     document.title = timerInterval === null
         ? 'Pomodoro Timer'
@@ -125,7 +159,7 @@ function manageTaskList(action){
         var taskDesc = inputTaskDesc.value;
         var taskStartTime = getCurrentTime();
         data = [{
-            period: taskStartTime + ' -',
+            period: taskStartTime + ' - ',
             task: taskDesc
         }].concat(data);
         
@@ -134,11 +168,16 @@ function manageTaskList(action){
         var currentEntry = data[0];
         currentEntry.period = currentEntry.period + getCurrentTime() + ' ... '; 
     } 
+    else if (action === 'resume'){
+        var currentEntry = data[0];
+        currentEntry.period = currentEntry.period + getCurrentTime() + ' - '; 
+    }
     else if (action === 'stop'){
         var currentEntry = data[0];
         currentEntry.period = currentEntry.period + getCurrentTime(); 
     }
 
+    storeDataInCookies();
     updateTaskList(data);
 }
 
@@ -182,20 +221,36 @@ function buildListElement(period, task){
     return li; 
 }
 
-/* <ul>
-    <li>
-        <div class="time-period">12:00 - 12:25</div>
-        <div class="wrapped-content-text">Reading book</div>
-    </li>
-    <li>
-        <div class="time-period">11:00 - 11:25</div>
-        <div class="wrapped-content-text">Watching tutorials</div>
-    </li>
-    <li>
-        <div class="time-period">10:00 - 10:25</div>
-        <div class="wrapped-content-text">Exercises</div>
-    </li>
-</ul> */
+function storeDataInCookies(){
+    var expDate = new Date();
+    expDate.setDate(expDate.getDate() + 1);
+    document.cookie = "data=" + JSON.stringify(data) + "; SameSite=Strict; path=/; expires=" + expDate.toUTCString();
+}
+
+function restoreDataFromCookies(){
+    var ca = document.cookie.split(';');
+    var localData = null;
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf("data") == 0) {
+            localData = c.substring("data".length,c.length);
+            break;
+        }
+    }
+
+    var newJson = localData.replace(/'/g, '"');
+
+    newJson = newJson.replace(/([^"]+)|("[^"]+")/g, function($0, $1, $2) {
+        if ($1) {
+            return $1.replace(/([a-zA-Z0-9]+?):/g, '"$1":');
+        } else {
+            return $2; 
+        } 
+    });
+
+    data = JSON.parse(newJson);
+}
 
 
 
